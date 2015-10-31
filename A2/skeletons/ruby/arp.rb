bfcb503f774a7d87c8bf339be4a28c4c85a8ba17
danReynolds <fnetma@hotmail.com>
2015-10-31 11:44:48 -0400

    changed arp and ddos

--- A2/skeletons/ruby/arp.rb
+++ A2/skeletons/ruby/arp.rb
@@ -1,13 +1,15 @@
 class Arp < Attack
   attr_reader :addresses
 
+  OP_REPLY = 2                                                                  # An Arp Operation Code of 2 indicates a REPLY
+
   def initialize(args)
     super
     @addresses = {}
   end
 
   def run(packet)
-    return unless arp_packet?(packet) && packet.arp_opcode == 2
+    return unless arp_packet?(packet) && packet.arp_opcode == OP_REPLY
     arp_reply = packet.arp_header.arp_src_mac_readable.upcase
     ip_reply = packet.arp_saddr_ip
     if @addresses[ip_reply] && @addresses[ip_reply] != arp_reply
--- A2/skeletons/ruby/ddos.rb
+++ A2/skeletons/ruby/ddos.rb
@@ -1,8 +1,9 @@
 class DDOS < Attack
   MON_GETLIST_1 = 42
+  NTP_PORT = 123
 
   def run(packet)
-    return unless udp_packet?(packet)
+    return unless udp_packet?(packet) && packet.udp_dst == NTP_PORT
 
     bytes = packet.udp_header.body.each_byte.to_a
     request_type = bytes[3]                                                     # The fourth byte contains the request code for the operation to be performed
