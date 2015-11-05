4fa88ff294f857f215d418df84bc79bcf9522b1d
danReynolds <fnetma@hotmail.com>
2015-11-05 15:44:02 -0500

    updated unicode, started typing answers

--- A2/skeletons/ruby/unicode.rb
+++ A2/skeletons/ruby/unicode.rb
@@ -9,9 +9,9 @@ class Unicode < Attack
   end
 
   def run(packet)
-    return unless full_packet?(packet) && packet.tcp_header.tcp_dst == HTTP_PORT
-
-    if @unicode_chars.any? { |chars| packet.tcp_header.body.downcase.include? chars }
+    return unless full_packet?(packet) && packet.tcp_header.body.length.nonzero? && packet.tcp_header.tcp_dst == HTTP_PORT # Only check the packet if it was a HTTP request
+    url = packet.tcp_header.body.split("\n").first.downcase                     # The URL is the first part of the body up until a newline
+    if @unicode_chars.any? { |chars| url.include? chars }
       @description = "src:#{packet.ip_saddr}, dst:#{packet.ip_daddr}"
       message
     end
