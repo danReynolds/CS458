69fe2b9939e73129b64b74a9bfea7746b4e99dd6
danReynolds <fnetma@hotmail.com>
2015-11-24 12:01:02 -0500

    updated to handle more than one byte of padding

--- A3/decrypt.rb
+++ A3/decrypt.rb
@@ -1,8 +1,10 @@
 require 'net/http'
 require 'base64'
 
-HTTP_OK = "200"
+HTTP_OK    = "200"
+HTTP_ERROR = "500"
 BLOCK_SIZE = 16
+BYTE_RANGE = (0..255).to_a
 blocks = Base64.decode64(ARGV[0]).bytes.each_slice(16).map(&:to_a)
 
 def send_request(cookie)
@@ -14,28 +16,55 @@ def send_request(cookie)
   http.request(request)
 end
 
-def decode_block(decrypt_block, cipher_block, padding)
-  hack_block = Array.new(BLOCK_SIZE, 0)
+def find_offset(test_block, decrypt_block, pos)
+  (BYTE_RANGE).detect do |v|
+    test_block[pos] = v
+    guess = Base64.encode64((test_block + decrypt_block).pack('c*'))
+    res = send_request(guess)
+    res.code == HTTP_OK
+  end
+end
+
+def calculate_padding(test_block, decrypt_block)
+  BLOCK_SIZE - BLOCK_SIZE.times.detect do |i|
+    test_block[i] ^= 1
+    guess = Base64.encode64((test_block + decrypt_block).pack('c*'))
+    res = send_request(guess)
+    res.code == HTTP_ERROR
+  end
+end
+
+def decode_block(decrypt_block, cipher_block)
+  hack_block = BYTE_RANGE.shuffle.first(16)
   encoded_bytes = Array.new(BLOCK_SIZE, 0)
+  padding_length = 0
 
   decrypt_block.reverse.each_with_index do |byte, index|
-    hack_block[BLOCK_SIZE - 1] = encoded_bytes.last ^ (index + 1)
-    encoded_bytes[BLOCK_SIZE - index - 1] = (0..255).to_a.detect do |v|
-      hack_block[BLOCK_SIZE - index - 1] = v
-      guess = Base64.encode64((hack_block + decrypt_block).pack('c*'))
-      res = send_request(guess)
-      res.code == HTTP_OK
+    pos = BLOCK_SIZE - index - 1
+    if index.zero?
+      hack_block[pos] = find_offset(hack_block.dup, decrypt_block, pos)
+      padding_length = calculate_padding(hack_block.dup, decrypt_block)
+      puts padding_length
+      encoded_bytes[pos] = hack_block[pos] ^ padding_length
+    elsif index < padding_length - 1
+      encoded_bytes[pos] = hack_block[pos]
+    else
+      hack_block[BLOCK_SIZE - 1] = encoded_bytes.last ^ (index + 1)
+      encoded_bytes[pos] = hack_block[pos] = find_offset(hack_block.dup, decrypt_block, pos)
     end
-    encoded_bytes[BLOCK_SIZE - 1] ^= 1 if index.zero?
   end
 
   cipher_block.zip(encoded_bytes).map do |cipher_byte, encoded_byte|
+    puts cipher_byte ^ encoded_byte
     cipher_byte ^ encoded_byte
   end.pack("c*")
 end
 
 decrypted_message = (blocks.length - 1).times.inject("") do |acc, i|
-  decode_block(blocks.pop, blocks.last, i == 0) + acc
-end[0..-2]
+  a = decode_block(blocks.pop, blocks.last)
+  puts a.bytes.map(&:to_a).join(",")
+  a + acc
+end
 
+puts decrypted_message.bytes.map(&:to_a).join(",")
 puts decrypted_message
